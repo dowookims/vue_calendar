2ea7f1acd68595d8308147585a3e34e508c5b08e
KimDoWoo <kaei3546@naver.com>
2019-10-01 17:46:42 +0900

    [ADD] Dates, Days Component with vue data / computed (Before use vuex)

---
+++ myapp-vue/src/components/Days.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="days--container">
+    <div 
+      class="days--text"
+      v-for="(day, idx) in days"
+      :key="idx"
+    >
+      {{ day }}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Days",
+    data(){
+      return {
+        days : ["일", "월", "화", "수", "목", "금", "토"]
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+.days--container {
+  height: 30px;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+
+.days--text {
+  padding: 6px 0;
+  font-weight: 600;
+  border-right: 0.4px solid #999;
+  border-left: 0.4px solid #999;
+  background-color: #BBB;
+  min-width: 14.285714%;
+  max-width: 14.285714%;
+  &:first-child {
+    color: red;
+  }
+  &:last-child {
+    color: royalblue;
+  }
+}
+</style>
