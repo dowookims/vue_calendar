fc879a0fa2be41d1f8a5128e638ce54899a93086
KimDoWoo <kaei3546@naver.com>
2019-10-01 17:44:31 +0900

    [MOD] Home.vue to add header component

---
+++ myapp-vue/src/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <Header />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from "@/components/Header";
+export default {
+  name: "home",
+  components: {
+    Header
+  }
+};
+</script>
