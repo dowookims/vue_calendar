08fde130a10d4a8fe440b1eb0b2212f405a8182b
KimDoWoo <kaei3546@naver.com>
2019-10-02 09:57:10 +0900

    [MOD] Make DateItem component and change state to vuex

--- myapp-vue/src/components/Header.vue
+++ myapp-vue/src/components/Header.vue
@@ -6,37 +6,46 @@
     </div>
     <div class="header__text-day--info">
       <p>{{ this.showKoreanDate }}요일</p>
-      <button @click="setOtherDay(1)">+</button>
-      <button @click="setOtherDay(-1)">-</button>
+      <button @click="changeMonth(1)">+</button>
+      <button @click="changeMonth(-1)">-</button>
     </div>
   </header>
 </template>
 
 <script>
-  import { mapGetters, mapMutations } from "vuex"
-  export default {
-    name: 'Header',
-    data(){
-      return {
-      }
+import { mapGetters, mapMutations } from "vuex";
+export default {
+  name: "Header",
+  methods: {
+    ...mapMutations("calendar",
+      [
+        "setBaseOtherDay",
+        "setBaseFirstDay",
+        "setBaseLastDate",
+        "setBaseLastMonthDate"
+      ]
+    ),
+    changeMonth(n){
+      this.setBaseOtherDay(n);
+      this.setBaseFirstDay();
+      this.setBaseLastDate();
+      this.setBaseLastMonthDate();
+    }
+  },
+  computed: {
+    ...mapGetters("calendar", ["today"]),
+    showDate(){
+      return this.today.getDate()
     },
-    methods: {
-      ...mapMutations("calendar", ["setOtherDay"])
+    showDetailDate(){
+      return `${this.today.getFullYear()}년 ${this.today.getMonth()+1}월`
     },
-    computed: {
-      ...mapGetters("calendar", ["today"]),
-      showDate(){
-        return this.today.getDate()
-      },
-      showDetailDate(){
-        return `${this.today.getFullYear()}년 ${this.today.getMonth()+1}월`
-      },
-      showKoreanDate(){
-        const dayArr = ["일", "월", "화", "수", "목", "금", "토"]
-        return dayArr[this.today.getDay()]
-      }
+    showKoreanDate(){
+      const dayArr = ["일", "월", "화", "수", "목", "금", "토"]
+      return dayArr[this.today.getDay()]
     }
   }
+}
 
 </script>
 
--- myapp-vue/src/store/modules/calendar.js
+++ myapp-vue/src/store/modules/calendar.js
@@ -1,23 +1,54 @@
 const state = {
   today: "",
-  year: 2019,
-  month: 10,
-  date: 1,
-  day: 2,
-  pinDay: ''
+  baseDay: "",
+  baseFirstDay: "",
+  baseLastDate: "",
+  baseLastMonthDate: ""
 };
 
 const getters = {
-  today: state => state.today
+  today: state => state.today,
+  baseDay: state => state.baseDay,
+  baseFirstDay: state => state.baseFirstDay,
+  baseLastDate: state => state.baseLastDate,
+  baseLastMonthDate: state => state.baseLastMonthDate
 };
 
 const actions = {};
 
 const mutations = {
   setToday: state => (state.today = new Date()),
-  setOtherDay: (state, payload) => 
-    ( state.today = new Date(state.today.getFullYear(), state.today.getMonth()+payload) )
-
+  setBaseDay: state => (state.baseDay = new Date()),
+  setBaseOtherDay: (state, payload) => {
+    state.baseDay = new Date(
+      state.baseDay.getFullYear(),
+      state.baseDay.getMonth() + payload
+    );
+  },
+  setBaseFirstDay: state => {
+    const monthFirst = new Date(
+      state.baseDay.getFullYear(),
+      state.baseDay.getMonth(),
+      1
+    );
+    state.baseFirstDay = monthFirst.getDay();
+  },
+  setBaseLastDate: state => {
+    const lastDate = new Date(
+      state.baseDay.getFullYear(),
+      state.baseDay.getMonth() + 1,
+      0
+    );
+    state.baseLastDate = lastDate.getDate();
+  },
+  setBaseLastMonthDate: state => {
+    const lastMonthLastDay = new Date(
+      state.baseDay.getFullYear(),
+      state.baseDay.getMonth(),
+      0
+    );
+    state.baseLastMonthDate = lastMonthLastDay.getDate();
+  }
 };
 
 export default {
