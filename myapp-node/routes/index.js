8f1714ff8f88799e6543ef4316fa7acd66241df3
dowookims <kaei2574@gmail.com>
2019-10-18 23:59:17 +0900

    [Delete]Node

--- myapp-node/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'programmers 과제 테스트 템플릿 - Node.js' });
-});
-
-module.exports = router;
